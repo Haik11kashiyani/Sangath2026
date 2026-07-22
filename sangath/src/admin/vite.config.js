2d0a3b79650125d781739dca3044614d47a7efb7
Hardik Kashiyani <hardik.kashiyani@tssconsultancy.com>
2026-07-22 13:31:20 +0530

    stepup for the hosting

--- sangath/src/admin/vite.config.js
+++ sangath/src/admin/vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
     host: '0.0.0.0'
   },
   build: {
-    outDir: path.resolve(__dirname, '../dist/admin'),
+    outDir: path.resolve(__dirname, '../../dist/admin'),
     emptyOutDir: true,
     rollupOptions: {
       input: path.resolve(__dirname, 'index.html')
